0fa6e347966f0320a31605fc504dfee0b5c70589
Angelika Cimene <angelikacimene@gmail.com>
2019-02-06 05:32:40 -0800

    studio2

---
+++ studio2/script.js
@@ -0,0 +1,24 @@
+// 'use strict';
+// console.log('reading js');
+//
+// var beanie = document.querySelector('#beanie');
+// var waterBottle = document.querySelector('#bottle');
+// var laptop = document.querySelector('#laptop');
+// var paragraph =document.querySelector('#paragraph');
+//
+//  beanie.addEventListener ('click', function() {
+//    paragraph.innerHTML= 'I can\'t live without my beanie. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+//    document.querySelector('#paragraph').style.position='fixed';
+//    document.querySelector('#paragraph').style.top='35rem';
+//    document.querySelector('#bottle').style.display='none';
+//    document.querySelector('#laptop').style.display='none';
+//  });
+//
+// waterBottle.addEventListener ('click', function() {
+//    paragraph.innerHTML= 'I can\'t live without my water bottle';
+//       document.querySelector('#paragraph').style.top='15em';
+//  });
+//
+// laptop.addEventListener ('click', function() {
+//     paragraph.innerHTML= 'I can\'t live without my laptop';
+//   });
