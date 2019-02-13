a81560ef93cdf72618cb1c2ffbef532c43a059fd
Angelika Cimene <angelikacimene@gmail.com>
2019-02-13 03:30:10 -0800

    studio2
    revised

--- studio2/script.js
+++ studio2/script.js
@@ -53,7 +53,7 @@ beanie.addEventListener('click', function() {
   document.getElementById('change').src = "images/beanie.png";
   document.querySelector('#paragraph').style.display = 'flex';
   title.innerHTML = 'Beanie';
-  text.innerHTML = 'My head needs warmth. I\’m such a mess. It\’s 6AM in the morning. I need some rest. I\’m looking for my beanie. It\’s not around. I thought it was in my closet. But it\’s nowhere to be found. I\’m going crazy. And freaking out. I sit in my desk. And forever pout.';
+  text.innerHTML = 'My head needs warmth and I\’m such a mess. It\’s 6AM in the morning and I need lots of rest. I\’m looking for my beanie. It\’s not around. I thought it was in my closet, but it\’s nowhere to be found. I\’m going crazy and freaking out. I sit in my desk and forever pout.';
   document.getElementById('image1').style.opacity = '0.15';
   document.getElementById('image2').style.opacity = '0.15';
 });
@@ -62,7 +62,7 @@ bottle.addEventListener('click', function() {
   document.getElementById('change').src = "images/bottle.png";
   document.querySelector('#paragraph').style.display = 'flex';
   title.innerHTML = 'Bottle';
-  text.innerHTML = 'I’m dehydrated. And I\’m stressed. I\’m pulling an all nighter. For my test. My Hydroflask is empty. I need to fill it up. I can’t find it anywhere. So I grabbed a cup. I poured some water. and walked to my room. I\’m so exhausted. But my styudying must resume.';
+  text.innerHTML = 'I’m dehydrated and I\’m stressed. I\’m pulling an all nighter for my test. My Hydroflask is empty. I need to fill it up. I can’t find it anywhere, so I grabbed a cup. I poured some water and walked to my room. I\’m so exhausted, but my stu(dying) must resume.';
   document.getElementById('image1').style.opacity = '0.15';
   document.getElementById('image2').style.opacity = '0.15';
 
@@ -72,7 +72,7 @@ laptop.addEventListener('click', function() {
   document.getElementById('change').src = "images/laptop.png";
   document.querySelector('#paragraph').style.display = 'flex';
   title.innerHTML = 'Laptop';
-  text.innerHTML = 'I use my laptop. Every single day. I use it for homework. I use it to play. I spend 80\% of my time. Looking at a screen. Especially at night. With my mug of some caffeine. I can\’t find my charger. And my laptop’s battery is dead. That\’s it, I\’m done for the day. I\’m going to bed';
+  text.innerHTML = 'I use my laptop every single day. I use it for homework. I use it to play. I spend 80\% of my time looking at a screen. Especially at night with a glass of caffeine. I can\’t find my charger and my laptop’s battery is dead. That\’s it, I\’m done for the day. I\’m going to bed';
   document.getElementById('image1').style.opacity = '0.15';
   document.getElementById('image2').style.opacity = '0.15';
 });
